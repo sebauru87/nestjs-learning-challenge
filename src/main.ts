a25fef57c44a1bdc876da1703107e908b80ffa7d
Sebastian Ferreira <sebastian.ferreira@dualbootpartners.com>
2023-05-19 11:08:09 -0300

    add swagger. Complete practice-4

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs Learning')
+    .setVersion('1.0')
+    .addTag('nest-learning')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
